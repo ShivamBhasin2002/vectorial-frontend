ebd7e3b8e75ba564ae5b2c7d98bfbbc09bad3bfe
Shivam Bhasin <shivambhasin@Shivams-MacBook-Pro.local>
2024-10-11 17:27:03 +0530

    feat: new domain

--- constants/restConstants.ts
+++ constants/restConstants.ts
@@ -1,2 +1,2 @@
-export const DATA_STORAGE_SERVICE_ENDPOINT = "https://xnya29yu85.execute-api.us-west-2.amazonaws.com/prod";
-export const CHAT_AGENT_API_ENDPOINT = "https://nwi18wng80.execute-api.us-west-2.amazonaws.com/prod/";
+export const DATA_STORAGE_SERVICE_ENDPOINT = "https://data-api.run-vectorial-api.xyz";
+export const CHAT_AGENT_API_ENDPOINT = "https://chat-api.run-vectorial-api.xyz";
