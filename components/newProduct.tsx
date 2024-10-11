4976292760df6f66b227f7b708f1e008ec72ee45
Shivam Bhasin <shivambhasin@Shivams-MacBook-Pro.local>
2024-10-12 02:57:36 +0530

    feat: new product panel updated

--- components/newProduct.tsx
+++ components/newProduct.tsx
@@ -21,26 +21,26 @@ const NewProduct = () => {
       )}
       <div
         className={clsx(
-          "h-screen bg-mixed-20 text-white flex flex-col items-center p-4 transition-all duration-300 ease-in-out fixed overflow-hidden right-[-800px] top-0 bottom-0 w-[800px] z-[100] gap-4",
+          "h-screen bg-white text-black flex flex-col items-center p-4 transition-all duration-300 ease-in-out fixed overflow-hidden right-[-800px] top-0 bottom-0 w-[800px] z-[100] gap-4",
           isNewProductPanelOpen ? "translate-x-[-800px]" : ""
         )}
       >
         <input
-          className="w-full rounded-2xl p-4 min-w-[672px] min-h-[24px] bg-surface-20 relative z-10 outline-none"
+          className="w-full rounded-2xl p-4 min-w-[672px] min-h-[24px] bg-offwhite border-grey border-2 relative z-10 outline-none"
           placeholder="Product name"
           onChange={(e) => {
             setProductName(e.target.value);
           }}
         />
         <textarea
-          className="w-full rounded-2xl p-4 min-w-[672px] min-h-[200px] bg-surface-20 relative z-10 outline-none flex-1"
+          className="w-full rounded-2xl p-4 min-w-[672px] min-h-[200px] bg-offwhite border-grey border-2 relative z-10 outline-none flex-1"
           placeholder="Product description"
           onChange={(e) => {
             setProductDescription(e.target.value);
           }}
         />
         <button
-          className="focus:outline-none hover:bg-primary-100 flex gap-2 justify-center items-center p-4 rounded-xl w-full bg-primary-60 h-12 overflow-hidden mt-auto"
+          className="focus:outline-none hover:bg-yellow/95 flex gap-2 justify-center items-center p-4 rounded-xl w-full bg-yellow h-12 overflow-hidden mt-auto font-bold"
           onClick={() => {
             createProduct({
               productName,
