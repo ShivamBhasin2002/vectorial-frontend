7f6488d6f2af24add07f9a1437bfcb70c55b39b1
Shivam Bhasin <shivambhasin@Shivams-MacBook-Pro.local>
2024-11-18 00:38:07 +0530

    feat: added some mroe check

--- components/productCard.tsx
+++ components/productCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Product } from "@constants/types/product";
+import { useProductStore } from "@store/productsStore";
 import { useRouter } from "next/navigation";
 
 interface ProductCardProps extends Product {
@@ -12,11 +13,13 @@ export const ProductCard: React.FC<ProductCardProps> = ({
   productId,
 }) => {
   const router = useRouter();
+  const { setSelectedProduct } = useProductStore();
   return (
     <div
       className="min-w-[30%] max-w-[30%] flex-1  bg-cream border-brown border shadow-md hover:scale-[0.98] transition-all p-4 rounded-2xl min-h-40 items-start cursor-pointer"
       onClick={() => {
         router.push(`/dashboard/product/${productId}`);
+        setSelectedProduct(productId, true);
       }}
     >
       <div className="flex gap-2">
